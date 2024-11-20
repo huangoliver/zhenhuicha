fff9bcdfd4cd6162c570635fd2450309b17971db
shuihuan <shuirong.huang@nokia-sbell.com>
2024-11-20 16:54:47 +0800

    Change way of check username and password
    %INT_NO_SW_CHANGE

--- login.js
+++ login.js
@@ -1,13 +1,15 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
+document.getElementById("loginForm").addEventListener("submit", async (event) => {
     event.preventDefault();
-
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    const USERNAME = "your_username"; // 替换为实际的自定义变量值
-    const PASSWORD = "your_username"; // 替换为实际的自定义变量值
+    const response = await fetch("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password })
+    });
 
-    if (username === USERNAME && password === PASSWORD) {
+    if (response.ok) {
         // 登录成功，设置会话存储标记
         sessionStorage.setItem("isLoggedIn", "true");
         window.location.href = "form.html";
