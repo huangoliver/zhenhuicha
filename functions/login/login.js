e6062ea741b4aec0d6ff776030c4145fce9ebb82
shuihuan <shuirong.huang@nokia-sbell.com>
2024-11-21 13:37:36 +0800

    Add new path
    %INT_NO_SW_CHANGE

--- functions/login/login.js
+++ functions/login/login.js
@@ -2,9 +2,9 @@ export async function onRequestPost(context) {
     try {
         const { username, password } = await request.json();
         if (username === context.env.USERNAME && password === context.env.PASSWORD) {
-            return new Response(JSON.stringify({ success: true, error: "Login successfully"}), { status: 200 });
+            return new Response(JSON.stringify({ success: true, error: "Login successfully" }), { status: 200 });
         }
-        return new Response(JSON.stringify({ success: false, error: "Invalid username or password!"}), { status: 403 });
+        return new Response(JSON.stringify({ success: false, error: "Invalid username or password!" }), { status: 403 });
     } catch (error) {
         console.error(error);
         return new Response(JSON.stringify({ success: false, error: error.message }), { status: 500 });
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit", async (event) =>
     const password = document.getElementById("password").value;
 
     try {
-        const response = await fetch("/api/login", {
+        const response = await fetch("/login/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password })
